1baa8edfbe5c1a1203b3b3ecf1382c449c34751f
Darren Boyd <dboyd@tapiocamobile.com>
2008-10-29 14:43:30 -0700

    Adding a script to get all of the bundles in a more manual process.

---
+++ update_bundles.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby
+
+
+# adding some cool magic I learned from merb :D
+class String
+  def /(other)
+    File.join(self, other)
+  end
+end
+
+bundles = {
+  'ruby-on-rails' => {:type => :git, :url => 'git://github.com/drnic/ruby-on-rails-tmbundle.git'},
+  'ack'           => {:type => :git, :url => 'git://github.com/protocool/ack-tmbundle.git'},
+  'git'           => {:type => :git, :url => 'git://github.com/timcharper/git-tmbundle.git'},
+  'rspec'         => {:type => :git, :url => 'git://github.com/dchelimsky/rspec-tmbundle.git'},
+  'ruby-shoulda'  => {:type => :git, :url => 'git://github.com/drnic/ruby-shoulda-tmbundle.git'},
+  'merb'          => {:type => :git, :url => 'git://github.com/drnic/merb-tmbundle.git'},
+  'Ruby'          => {:type => :svn, :url => 'http://macromates.com/svn/Bundles/trunk/Bundles/Ruby.tmbundle'}
+}
+
+
+
+
+base_dir = ENV['HOME'] / "Library/Application Support/TextMate"
+
+unless File.exist?(base_dir)
+  puts "There is no TextMate directory: #{base_dir}"
+  exit(1)
+end
+
+def update_svn(dir, url)
+  if File.exist?(dir) && File.exist?(dir / '.svn')
+    puts `cd "#{dir}" && svn up`
+    true
+  elsif !File.exist?(dir)
+    puts `svn co "#{url}" "#{dir}"`
+    true
+  else
+    nil
+  end
+end
+
+def update_git(dir, url)
+  if File.exist?(dir) && File.exist?(dir / '.git')
+    puts `cd "#{dir}" && git pull`
+    true
+  elsif !File.exist?(dir)
+    puts `git clone "#{url}" "#{dir}"`
+    true
+  else
+    nil
+  end
+end
+
+puts "Updating the Support folder."
+unless update_svn(base_dir / 'Support', 'http://macromates.com/svn/Bundles/trunk/Support')
+  puts "You seem to have a Support directory not under Subversion source control."
+  puts "Sorry, I can't deal with that."
+end
+puts ""
+
+require 'fileutils'
+FileUtils.mkdir_p(base_dir/'Bundles')
+
+bundles.each do |name, bundle|
+  puts "Updating Bundle: #{name}"
+  case bundle[:type]
+  when :git, :svn
+    unless send("update_#{bundle[:type]}", base_dir/'Bundles'/"#{name}.tmbundle", bundle[:url])
+      puts "You seem to have a Bundle called #{name} already, but not under source control (#{bundle[:type]}).  Skipping."
+    end
+  else
+    "Error with bundle #{name}, unkown type."
+  end
+end
